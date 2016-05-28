673cac20f5b0a3a86e7d64c89e8f95d3e9c85c82
Andrew-DiBella <adibella@ufl.edu>
2016-05-28 10:45:56 -0700

    add additional widgets to function.php

--- functions.php
+++ functions.php
@@ -12,5 +12,28 @@ function blank_widgets_init() {
 		'after_title' => '</h2>'
 		));
 	}
+
+	/*--- Second New Widget --- */
+    register_sidebar( array(
+        'name' => ('Footer Widget Two'),
+        'id' => 'footer-widget-two',
+        'description' => 'Second widget for our footer',
+        'before_widget' => '<div class="widget-footer">',
+        'after_widget' => '</div>',
+        'before_title' => '<h2>',
+        'after_title' => '</h2>'
+        ));
+
+    register_sidebar( array(
+        'name' => ('Footer Widget Three'),
+        'id' => 'footer-widget-three',
+        'description' => 'Third widget for our footer',
+        'before_widget' => '<div class="widget-footer">',
+        'after_widget' => '</div>',
+        'before_title' => '<h2>',
+        'after_title' => '</h2>'
+        ));
+
+}
 add_action('widgets_init', 'blank_widgets_init');
  ?>

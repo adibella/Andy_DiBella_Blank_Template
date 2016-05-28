0fd25823c4d3b6b265e95a76c5c90763715dfdd9
Andrew-DiBella <adibella@ufl.edu>
2016-05-28 10:57:42 -0700

    add widget content to footer.php

--- footer.php
+++ footer.php
@@ -1,10 +1,19 @@
 <!-- BEGINNING OF FOOTER - END SECTION CONTENT -->
 			<footer class="row">
-				<div class="twelve columns">
-					<p>Footer content goes here.</p>
+    			<div class="four columns">
+					<?php dynamic_sidebar('footer-widget-one'); ?>
+				</div>
+
+    			<div class="four columns">
+					<?php dynamic_sidebar('footer-widget-two'); ?>
+				</div>
+
+				<div class="four columns">
+					<?php dynamic_sidebar('footer-widget-three'); ?>
 				</div>
 			</footer>
 		</div> <!-- ENDS CONTAINER -->
 	<?php wp_footer(); ?>
+
 	</body>
 </html>

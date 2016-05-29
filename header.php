b4e61889fb27d0c9cfa3efe90abb8d1bbf9d75cb
Andrew-DiBella <adibella@ufl.edu>
2016-05-29 16:36:53 -0700

    add menu

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html>
 	<head>
 		<title>Wordpress Template</title>
-<?php wp_head(); ?>
+		<?php wp_head(); ?>
 		<!-- Links to styles.css -->
 		<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" />
 	</head>
@@ -11,8 +11,10 @@
 		<div class="container">
 			<header class="row">
 				<div class="twelve columns">
-					<h1>Blank WordPress Template</h1>
-					<p>This is my WordPress template.</p>
+					<?php wp_nav_menu(array(
+						'sort_column' => 'menu_order',
+						'container_class' => 'blank-menu-header'
+					));?>
 				</div>
 			<header/>
 			<!-- END OF HEADER - BEGIN SECTION CONTENT -->
